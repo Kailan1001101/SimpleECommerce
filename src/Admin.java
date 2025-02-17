dfa38639a818bed9ca2b270212a80264bb9e35e9
Kailan <Kailan1001101@gmail.com>
2025-02-17 22:35:35 +0200

    Admin Changes

---
+++ src/Admin.java
@@ -0,0 +1,20 @@
+public class Admin extends Users{
+    public Admin(String name, String surname, String email, String Password) {
+        super(name, surname, email, Password);
+
+    }
+
+    public void addProduct(){
+
+    }
+
+    public void removeProduct(){
+
+    }
+    public void removeCustomer(){
+
+    }
+    public void addCustomer(){
+
+    }
+}
